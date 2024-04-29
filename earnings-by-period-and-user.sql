cb6c51392d6e6273c5e330365581468b3ca87d58
pzambao <pedro.leite@totalip.com.br>
2024-04-29 20:03:46 -0300

    earnings by period query

---
+++ earnings-by-period-and-user.sql
@@ -0,0 +1,14 @@
+-- Rendimento por período e usuário
+
+SELECT
+  user,
+  date_trunc('month', transaction_date) AS period,
+  SUM(transaction_value) AS total_earnings
+FROM
+  transaction_table
+WHERE
+  transaction_date BETWEEN '2024-01-01' AND '2024-12-31'
+GROUP BY
+  user, date_trunc('month', transaction_date)
+ORDER BY
+  user, period;
